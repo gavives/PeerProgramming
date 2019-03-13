bae3686be5a0692d3b1d8c2545730b49ae763603
Gavin Ives <gavinives@live.com>
2019-03-13 10:44:37 -0400

    End of Wednesday

--- PeerProgramming Space Game/Program.cs
+++ PeerProgramming Space Game/Program.cs
@@ -6,10 +6,27 @@ using System.Threading.Tasks;
 
 namespace PeerProgramming_Space_Game
 {
-    class Program
+    class Atom
     {
-        static void Main(string[] args)
+        public int weapons; //fuel consumption
+        public double money; // money exchange rate
+        public int ammo; // names the planet
+        public bool isAlive; // set visual and other fun stuff 
+        public string spaceship;
+
+
+        public Atom()
         {
         }
+
+        public Atom(int weapons, double money, int ammo, bool isAlive, string spaceship)
+        {
+            this.weapons = weapons;
+            this.money = money;
+            this.ammo = ammo;
+            this.isAlive = isAlive;
+            this.spaceship = spaceship;
+        }
+
     }
 }
