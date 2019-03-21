f9cc1cf4e793a66de382251c9360856353c7ea7e
Joel-Crunk <joel.crunk@outlook.com>
2019-03-21 10:07:25 -0400

    Set current planet with Elliot's help

--- PeerProgramming Space Game/Planet.cs
+++ PeerProgramming Space Game/Planet.cs
@@ -8,47 +8,35 @@ namespace PeerProgramming_Space_Game
 {
     class Planet
     {
-        private int age; //fuel consumption
-        private double money; // money exchange rate
-        private string name; // names the planet
-        private bool isProsperous; // set visual and other fun stuff 
+        private readonly List<Planet> galaxy;
+        private Planet currentPlanet;
 
-        public int Age { get => age; set => age = value; }
-        public double Money { get => money; set => money = value; }
-        public string Name { get => name; set => name = value; }
-        public bool IsProsperous { get => isProsperous; set => isProsperous = value; }
+        public int    Age          { get; set; } = 25;
+        public double Money        { get; set; }
+        public string Name         { get; set; }
+        public bool   IsProsperous { get; set; }
 
         public Planet()
         {
+            galaxy = NewPlanetList(this);
+            currentPlanet = galaxy[0];
         }
 
         public Planet(int age, double money, string name, bool isProsperous)
         {
 
-            this.age = age;
+            this.Age = age;
             this.Money = money;
             this.Name = name;
             this.IsProsperous = isProsperous;
         }
+
         public void PlanetList()
         {
-            
             Atom atom = new Atom();
-            Planet planet = new Planet();
-            //Random random = new Random();
 
-
-
-            //// random.Next(0, 3);
-            //int myRandom = random.Next(0, 10);
-
-
-            List<Planet> galaxy = new List<Planet>
-            {
-                new Planet(1, 1, "Earth", planet.IsProsperous),
-                new Planet(2, 2, "Pluto", planet.IsProsperous),
-                new Planet(10, 2.5, "Waconda", planet.IsProsperous)
-            };
+            int myRandom = RandomNumber();
+            ////////////////////////////
 
             void CenteredString(string s)
             {
@@ -63,30 +51,78 @@ namespace PeerProgramming_Space_Game
                 }
             }
 
-           
-                Console.WriteLine("\n");
-                CenteredString("What planet do you want to go to?");
-                CenteredString("1. Earth");
-                CenteredString("2. Pluto");
-                CenteredString("  3. Waconda");
-                
+
+            Console.WriteLine("\n");
+            CenteredString("What planet do you want to go to?");
+            CenteredString("1. Earth");
+            CenteredString("2. Pluto");
+            CenteredString("  3. Waconda");
+
 
             int travel = int.Parse(Console.ReadLine());
-                travel -= 1;
-                galaxy.ToArray();
-                CenteredString($"You are headed to {galaxy[travel].Name}");
-                Age += galaxy[travel].age;
+            travel -= 1;
+
+            //AddProsperity();
+            CenteredString($" You are at {currentPlanet.Name}");
+            CenteredString($"You are headed to {galaxy[travel].Name}");
+
+            currentPlanet = galaxy[travel];
+            Age += galaxy[travel].Age;
+
             Console.WriteLine(atom.age);
             atom.age += Age;
-            Console.WriteLine(atom.age);
+
+            Console.WriteLine($"{atom.age} years old");
             Console.WriteLine("");
             CenteredString($" Your age is now {atom.age}!");
+
+            if (myRandom % 3 == 0)
+            {
+                CenteredString("This Planet has high prices!");
+                double loseMoney = (myRandom * 2);
+                Money -= galaxy[travel].Money;
+                Money -= loseMoney;
+                CenteredString($"You lose {loseMoney}");
+            }
+            else
+            {
+                CenteredString("You make money!");
+                double makemoney = (myRandom + 50);
+                Money += galaxy[travel].Money;
+                Money += makemoney;
+                CenteredString($"You make {makemoney}");
+            }
+
+            CenteredString($"${Money}");
+           
+
             TravelToPlanet();
 
-
         }
-        public void TravelToPlanet()
+
+        private static int RandomNumber()
         {
+            Random random = new Random();
+            //random.Next(0, 3);
+            int myRandom = random.Next(1, 101);
+            Console.WriteLine($"{myRandom} is the random number");
+            return myRandom;
+        }
+
+        private static List<Planet> NewPlanetList(Planet planet)
+        {
+            return new List<Planet>
+            {
+                new Planet(1, 1, "Earth", planet.IsProsperous),
+                new Planet(2, 2, "Pluto", planet.IsProsperous),
+                new Planet(10, 2.5, "Waconda", planet.IsProsperous)
+            };
+        }
+
+
+         public void TravelToPlanet()
+        {
+           
             Atom atom = new Atom();
             void CenteredString(string s)
             {
@@ -105,7 +141,6 @@ namespace PeerProgramming_Space_Game
             CenteredString("Travel to a Planet?");
             Console.ForegroundColor = ConsoleColor.Yellow;
             CenteredString("Y. yes");
-            Console.ForegroundColor = ConsoleColor.Cyan;
             CenteredString("N. no");     
             CenteredString("");
          
@@ -131,18 +166,9 @@ namespace PeerProgramming_Space_Game
                 {
                     PlanetList();
                 }
-
-
+                
             }
-             
-         
-            
-
-
-           // atom.age += Planet.Current - PlanetList;
-
-           
+            // atom.age += Planet.Current - PlanetList;  
         }
-
     }
 }
